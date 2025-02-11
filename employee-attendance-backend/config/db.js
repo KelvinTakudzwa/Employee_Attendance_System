80d0ec5120e5680243da4670c1f12cdfe7de77f4
KelvinTakudzwa <takukelvin01@gmail.com>
2025-02-11 14:43:30 +0200

    changes

--- employee-attendance-backend/config/db.js
+++ employee-attendance-backend/config/db.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: 'root',
-  password: '0786682192@Tk',
-  database: 'employee_attendance',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
